5e9d5bfeeb7afea4ae9bc9418266d7188d17e309
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-26 11:21:42 +0100

    Fix csrf token

--- things/forms.py
+++ things/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import User, Thing
 from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 
 class ThingForm(forms.ModelForm):
     class Meta:
@@ -10,6 +12,28 @@ class ThingForm(forms.ModelForm):
         'description' : forms.Textarea(),
         'quantity' : forms.NumberInput()
         }
+
+    name = forms.CharField(
+        validators=[RegexValidator(
+            regex=r'^.*{0,35}$',
+            message='Maximum length is 35.'
+            )]
+    )
+
+    description = forms.CharField(
+        validators=[RegexValidator(
+            regex=r'^.*{0,120}$',
+            message='Description can be blank and up to 120 characters.'
+            )]
+    )
+
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(50),
+            RegexValidator(
+            regex=r'/^(?:[1-9]|[1-4][0-9]|50)$/',
+            message='Number should be between 0 and 50.'
+            )]
+    )
 
 
 class SignUpForm(forms.ModelForm):
