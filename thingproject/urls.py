05adab8357cbef5f1a85b04feaf70b30e1704103
Ezzat Alsalibi <ezzat.alsalibi@kcl.ac.uk>
2022-10-26 11:47:38 +0100

    Fix reg expressions

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -19,7 +19,8 @@ from things import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('things_form/', views.things_form, name='things_form'),
+    #path('', views.home, name='home'),
+    #path('things_form/', views.things_form, name='things_form'),
+    path('', views.things_form, name='things_form'),
     path('sign_up/', views.sign_up, name='sign_up'),
 ]
